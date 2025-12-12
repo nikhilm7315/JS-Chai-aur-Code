8e6dab910ce436e131c168025e9753694316445c
Nikhil Mishra <nilhilmishra7315@gmail.com>
2025-12-12 14:37:35 +0000

    updated operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -6,9 +6,40 @@
 // console.log(typeof userInNumber)
 
 let isLoggedIn= 1
-console.log(typeof isLoggedIn)
-console.log(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(isLoggedIn)
 
 let valueInBoolean= Boolean(isLoggedIn)
-console.log(typeof valueInBoolean)
-console.log(valueInBoolean)
+// console.log(typeof valueInBoolean)
+// console.log(valueInBoolean)
+
+
+// *********Operations*********
+
+let value=3
+let negvalue= -value
+//console.log(negvalue)
+
+let str1 = "nikhil "
+let str2 = "mishra"
+
+let str= str1 + str2
+// console.log(str)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + "2");
+// console.log("1" + "2" + 2);
+// console.log(1 + 2 + "2");
+// console.log("1"+ 2 + 2);
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.table([num1, num2, num3])
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter)
