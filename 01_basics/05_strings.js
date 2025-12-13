66a48b8fea6d9dc00e529843986ac507b786c67c
Nikhil Mishra <nilhilmishra7315@gmail.com>
2025-12-13 14:48:45 +0000

    updated files

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,34 @@
-const name = "Nikhil"
-const myrepoCount = 3
+// const name = "Nikhil"
+// const myrepoCount = 3
 
-console.log(`My name is ${name} and my repo count is ${myrepoCount}`);
+//console.log(`My name is ${name} and my repo count is ${myrepoCount}`);
+
+// const gameName = new String ('GTAV') // **New way to define string
+// // console.log(typeof gameName);
+ 
+// console.log(gameName[3]);
+// // console.log(gameName.__proto__)
+
+// // console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.)
+
+let mac = "AirM4"
+console.log(mac.indexOf("M"))
+
+console.log(mac.slice(-5, 3))
+console.log(mac.substring(0, 4));
+
+
+const newString = "   Hero   "
+console.log(newString)
+console.log(newString.trim())
+
+const url ="https://nikhil.com/nikhil%07mishra"
+
+console.log(url.replace("%07", "-"))
+
+let anotherString = ("Mac-04")
+console.log(anotherString.split('-'))
+
+console.log(anotherString.bold())
