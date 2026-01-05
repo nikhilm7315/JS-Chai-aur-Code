d50bdb970ca90537476a629c8439af7e8804b5db
Nikhil Mishra <nilhilmishra7315@gmail.com>
2026-01-05 12:16:47 +0000

    edited

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 // array
 
-// const myArr = [0, 1, 2, 3, 4, 5];
+const myArr = [0, 1, 2, 3, 4, 5];
 // const myHeros = ["ironman", "captainamerica"];
 // const myArr2 = new Array(1, 2, 3, 4);
 
@@ -35,5 +35,6 @@ console.log(myn1);
 
 console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
 console.log(myn2);
+console.log("C ", myArr);
+
