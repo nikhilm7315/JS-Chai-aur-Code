d754b9c7d4b30794523df3d5df3acd2cc0ccffcd
Nikhil Mishra <nilhilmishra7315@gmail.com>
2026-02-20 18:37:23 +0000

    Studied Array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -13,4 +13,21 @@ const allHeroes = marvelHeroes.concat(dcHeroes) //This is good practice, it join
 // even more good operator is spread operator
 
 const allNewheroes = [...marvelHeroes, ...dcHeroes] // This ... frees each elements of an array into single element
-console.log(allNewheroes);
+// console.log(allNewheroes);
+
+const another_array = [4, 5, 8, [1, 4, 9, [8, 5, 1, [9, 4, 7]]], 9, 0]
+const realanother_array = another_array.flat(2) //The given arguement shows the depth of the array it opened, we can give till infinity too
+// console.log(realanother_array);
+
+
+// console.log(Array.isArray("Nikhil"))
+// console.log(Array.from("Nikhil"))
+
+// console.log(Array.from({name: "Nikhil"})) //Intresting (It will give an empty error untill defined if the key is to be made as array or the arguement)
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+const scorearray = Array.of(score1, score2, score3)
+console.log(scorearray)
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,3 @@
+// There are two ways in which an object can be declared
+// 1. Constructor(Singleton Object is formed) | 2. Literals
+
