f9817979cedb3c5870a238ab7aa17382590e2408
Nikhil Mishra <nilhilmishra7315@gmail.com>
2026-03-10 05:54:54 +0000

    studied arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,16 +8,63 @@ const user = {
     }
 }
 
-// user.welcomeMessage(); // This will print "Nikhil welcome to our website! Your price
+// user.welcomeMessage(); // This will print "Nikhil welcome to our website! Your price is
 
 // user.username = "Akhil";
 // user.welcomeMessage(); // This will print "Akhil welcome to our website! Your price is 1000"
 
-console.log(this); // In the global scope, 'this' refers to the global object (window in browsers, global in Node.js). In strict mode, 'this' will be undefined in the global scope.
+// console.log(this); // In the global scope, 'this' refers to the global object (window in browsers, global in Node.js). In strict mode, 'this' will be undefined in the global scope.
 
 
-function chai() {
-    console.log(this); // In a regular function, 'this' refers to the global object (window in browsers, global in Node.js). In strict mode, 'this' will be undefined in a regular function.
+// function chai() {
+//     let username = "abc"
+//     console.log(this.username); // In a regular function, 'this' refers to the global object (window in browsers, global in Node.js). In strict mode, 'this' will be undefined in a regular function.
+// }
+
+// chai()
+
+// ++++++++++++++++++++++++ Arrow Functions ++++++++++++++++++++++++++
+
+const chai = () => {
+    let username = "abc"
+    console.log(this);
 }
 
-chai();
+// chai();
+
+// const addtwo = (num1, num2) => {
+// return num1 + num2
+// }
+
+// Implicit Return
+// const addtwo = (num1, num2) => num1 + num2
+
+// const addtwo = (num1, num2) => (num1 + num2) //If written inside curly brackets then there's no need to write return
+// console.log(addtwo(3,4))
+
+const addtwo = (num1, num2) => ({username: "nikhil"})
+// console.log(addtwo())
+
+
+const myArr = [1, 3, 7, 9]
+
+// myArr.forEach(() => ())
+// myArr.forEach(() => {})
+
+
+// function greetings (){
+
+// }
+
+const greetings = (name) => {
+    console.log(`${name}`, "we welcome you.")
+
+}
+
+// console.log(greetings("nikhil"))
+
+let calculateSquare = (num) => {
+  return Math.pow(num);
+}
+
+console.log(calculateSquare(5))
